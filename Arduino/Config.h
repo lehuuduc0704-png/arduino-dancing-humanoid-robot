5ee8c6733339fd9d299b0d70433763cb3a0ad330
lehuuduc0704-png <lehuuduc0704@gmail.com>
2026-07-10 10:55:22 +0700

    Add Config file

---
+++ Arduino/Config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Servo Pins
+const byte HEAD_SERVO = 3;
+const byte LEFT_ARM_SERVO = 5;
+const byte RIGHT_ARM_SERVO = 6;
+const byte LEFT_WING_SERVO = 9;
+const byte RIGHT_WING_SERVO = 10;
+
+// Devices
+const byte SOUND_SENSOR = 2;
+const byte BUZZER = 8;
+const byte CHEST_LED = 11;
+
+#endif
