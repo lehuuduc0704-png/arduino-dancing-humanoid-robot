10b54945c6c6aac1368b42150a47243a4438ead7
lehuuduc0704-png <lehuuduc0704@gmail.com>
2026-07-10 11:00:28 +0700

    Create Dance header

---
+++ Arduino/Dance.h
@@ -0,0 +1,26 @@
+#ifndef DANCE_H
+#define DANCE_H
+
+class Dance
+{
+public:
+
+    void startup();
+
+    void greeting();
+
+    void idle();
+
+    void dance1();
+
+    void dance2();
+
+    void dance3();
+
+    void dance4();
+
+    void dance5();
+
+};
+
+#endif
