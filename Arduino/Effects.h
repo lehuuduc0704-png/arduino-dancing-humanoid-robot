0f3a9c4f4586acc74a75b3daf2c0b9aa9f6b5cad
lehuuduc0704-png <lehuuduc0704@gmail.com>
2026-07-10 10:58:55 +0700

    Create Effects header

---
+++ Arduino/Effects.h
@@ -0,0 +1,18 @@
+#ifndef EFFECTS_H
+#define EFFECTS_H
+
+class Effects
+{
+public:
+
+    void ledOn();
+
+    void ledOff();
+
+    void blink(int times);
+
+    void beep(int times);
+
+};
+
+#endif
