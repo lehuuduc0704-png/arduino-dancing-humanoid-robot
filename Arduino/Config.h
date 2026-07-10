a23fe8f005b89d44ab1b9b343e59ba9d4b612f62
lehuuduc0704-png <lehuuduc0704@gmail.com>
2026-07-10 14:56:30 +0700

    Improve Config

--- Arduino/Config.h
+++ Arduino/Config.h
@@ -1,16 +1,37 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+//========================
 // Servo Pins
-const byte HEAD_SERVO = 3;
-const byte LEFT_ARM_SERVO = 5;
-const byte RIGHT_ARM_SERVO = 6;
-const byte LEFT_WING_SERVO = 9;
-const byte RIGHT_WING_SERVO = 10;
+//========================
 
+const byte HEAD_PIN = 3;
+const byte LEFT_ARM_PIN = 5;
+const byte RIGHT_ARM_PIN = 6;
+const byte LEFT_WING_PIN = 9;
+const byte RIGHT_WING_PIN = 10;
+
+//========================
 // Devices
-const byte SOUND_SENSOR = 2;
-const byte BUZZER = 8;
-const byte CHEST_LED = 11;
+//========================
+
+const byte SOUND_PIN = 2;
+const byte BUZZER_PIN = 8;
+const byte CHEST_LED_PIN = 11;
+
+//========================
+// Default Position
+//========================
+
+const int HEAD_CENTER = 90;
+
+const int LEFT_ARM_CENTER = 90;
+const int RIGHT_ARM_CENTER = 90;
+
+const int LEFT_WING_CLOSE = 20;
+const int RIGHT_WING_CLOSE = 160;
+
+const int LEFT_WING_OPEN = 90;
+const int RIGHT_WING_OPEN = 90;
 
 #endif
