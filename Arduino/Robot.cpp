cc6c74719d00e4259bd81501f4835112c2ffffac
lehuuduc0704-png <lehuuduc0704@gmail.com>
2026-07-17 09:39:34 +0700

    Update Robot.cpp

--- Arduino/Robot.cpp
+++ Arduino/Robot.cpp
@@ -1,16 +1,37 @@
 #include "Robot.h"
 
 Robot::Robot()
-{
 
-}
+    : head(
+        HEAD_PIN,
+        HEAD_LEFT,
+        HEAD_RIGHT,
+        HEAD_CENTER),
 
-void Robot::begin()
-{
+      leftArm(
+        LEFT_ARM_PIN,
+        LEFT_ARM_UP,
+        LEFT_ARM_DOWN,
+        LEFT_ARM_CENTER),
 
-}
+      rightArm(
+        RIGHT_ARM_PIN,
+        RIGHT_ARM_DOWN,
+        RIGHT_ARM_UP,
+        RIGHT_ARM_CENTER),
 
-void Robot::update()
+      leftWing(
+        LEFT_WING_PIN,
+        LEFT_WING_CLOSE,
+        LEFT_WING_OPEN,
+        LEFT_WING_CENTER),
+
+      rightWing(
+        RIGHT_WING_PIN,
+        RIGHT_WING_OPEN,
+        RIGHT_WING_CLOSE,
+        RIGHT_WING_CENTER)
+
 {
 
 }
