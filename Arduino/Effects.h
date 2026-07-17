9cbd859c3c645ee2da40f58fa6dafb25a42a0ff7
lehuuduc0704-png <lehuuduc0704@gmail.com>
2026-07-17 09:45:16 +0700

    Update Effects.h

--- Arduino/Effects.h
+++ Arduino/Effects.h
@@ -4,15 +4,19 @@
 class Effects
 {
 public:
-
     void ledOn();
-
     void ledOff();
-
     void blink(int times);
-
     void beep(int times);
 
+private:
+    ServoController head;
+    ServoController leftArm;
+    ServoController rightArm;
+    ServoController leftWing;
+    ServoController rightWing;
+    ServoController* servos[5];
+    Effects effects;
 };
 
 #endif
