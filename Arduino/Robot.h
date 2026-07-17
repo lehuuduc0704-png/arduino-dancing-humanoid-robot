83facd7b9c41db4d969e616c1d6c3bf6d36ce8c5
lehuuduc0704-png <lehuuduc0704@gmail.com>
2026-07-17 09:35:31 +0700

    Update Robot.h

--- Arduino/Robot.h
+++ Arduino/Robot.h
@@ -1,8 +1,24 @@
 #ifndef ROBOT_H
 #define ROBOT_H
 
+#include <Arduino.h>
+
+#include "ServoController.h"
+#include "Config.h"
+#include "Effects.h"
+
 class Robot
 {
+private:
+
+    ServoController head;
+    ServoController leftArm;
+    ServoController rightArm;
+    ServoController leftWing;
+    ServoController rightWing;
+
+    Effects effect;
+
 public:
 
     Robot();
@@ -11,6 +27,19 @@ public:
 
     void update();
 
+    void startup();
+
+    void greeting();
+
+    void idle();
+
+    void dance1();
+
+    void dance2();
+
+    void dance3();
+
+    void randomDance();
 };
 
 #endif
