fa3948986253052978bfcfbcc581778104e52eed
lehuuduc0704-png <lehuuduc0704@gmail.com>
2026-07-17 09:37:22 +0700

    Update Config.h

--- Arduino/Config.h
+++ Arduino/Config.h
@@ -1,37 +1,72 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-//========================
+//====================================================
+// Pin Configuration
+//====================================================
+
 // Servo Pins
-//========================
+#define HEAD_PIN            3
+#define LEFT_ARM_PIN        5
+#define RIGHT_ARM_PIN       6
+#define LEFT_WING_PIN       9
+#define RIGHT_WING_PIN      10
 
-const byte HEAD_PIN = 3;
-const byte LEFT_ARM_PIN = 5;
-const byte RIGHT_ARM_PIN = 6;
-const byte LEFT_WING_PIN = 9;
-const byte RIGHT_WING_PIN = 10;
-
-//========================
 // Devices
-//========================
+#define SOUND_PIN           2
+#define BUZZER_PIN          8
+#define CHEST_LED_PIN       11
 
-const byte SOUND_PIN = 2;
-const byte BUZZER_PIN = 8;
-const byte CHEST_LED_PIN = 11;
+//====================================================
+// Head Positions
+//====================================================
 
-//========================
-// Default Position
-//========================
+#define HEAD_LEFT           60
+#define HEAD_CENTER         90
+#define HEAD_RIGHT          120
 
-const int HEAD_CENTER = 90;
+//====================================================
+// Left Arm Positions
+//====================================================
 
-const int LEFT_ARM_CENTER = 90;
-const int RIGHT_ARM_CENTER = 90;
+#define LEFT_ARM_UP         30
+#define LEFT_ARM_CENTER     90
+#define LEFT_ARM_DOWN       150
 
-const int LEFT_WING_CLOSE = 20;
-const int RIGHT_WING_CLOSE = 160;
+//====================================================
+// Right Arm Positions
+//====================================================
 
-const int LEFT_WING_OPEN = 90;
-const int RIGHT_WING_OPEN = 90;
+#define RIGHT_ARM_UP        150
+#define RIGHT_ARM_CENTER    90
+#define RIGHT_ARM_DOWN      30
+
+//====================================================
+// Left Wing Positions
+//====================================================
+
+#define LEFT_WING_CLOSE     20
+#define LEFT_WING_CENTER    55
+#define LEFT_WING_OPEN      90
+
+//====================================================
+// Right Wing Positions
+//====================================================
+
+#define RIGHT_WING_CLOSE    160
+#define RIGHT_WING_CENTER   125
+#define RIGHT_WING_OPEN     90
+
+//====================================================
+// Sound Sensor
+//====================================================
+
+#define SOUND_THRESHOLD     500
+
+//====================================================
+// Motion
+//====================================================
+
+#define DEFAULT_SPEED       8
 
 #endif
