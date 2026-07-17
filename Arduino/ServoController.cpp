b41d52dd70f2408cfb39f64f63022d54c6821df8
lehuuduc0704-png <lehuuduc0704@gmail.com>
2026-07-17 09:20:17 +0700

    Update ServoController.cpp

--- Arduino/ServoController.cpp
+++ Arduino/ServoController.cpp
@@ -1,31 +1,15 @@
 #include "ServoController.h"
 
-void ServoController::attach(int pin)
+ServoController::ServoController(
+    int pin,
+    int minAngle,
+    int maxAngle,
+    int centerAngle)
 {
-    servo.attach(pin);
-}
+    this->pin = pin;
+    this->minAngle = minAngle;
+    this->maxAngle = maxAngle;
+    this->centerAngle = centerAngle;
 
-void ServoController::write(int angle)
-{
-    servo.write(angle);
-}
-
-void ServoController::moveSmooth(int from,int to,int speed)
-{
-    if(from<to)
-    {
-        for(int i=from;i<=to;i++)
-        {
-            servo.write(i);
-            delay(speed);
-        }
-    }
-    else
-    {
-        for(int i=from;i>=to;i--)
-        {
-            servo.write(i);
-            delay(speed);
-        }
-    }
+    currentAngle = centerAngle;
 }
